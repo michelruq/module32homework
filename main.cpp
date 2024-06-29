80e81ed2a14d76d617e36c21deda17bf857e6b9b
Mike Chashchin <michelruq@mail.ru>
2024-06-29 18:13:43 +0500

    update for first iteration

--- main.cpp
+++ main.cpp
@@ -4,6 +4,84 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+class DBConnectionInterface
+{
+public:
+    DBConnectionInterface()
+    {}
+
+    virtual ~DBConnectionInterface()
+    {}
+
+    virtual void Open() = 0;
+    virtual void Close() = 0;
+    virtual void execQuery() = 0;
+};
+
+class classThatUsesDB
+{
+public:
+    classThatUsesDB(DBConnectionInterface* dbConnection) : usedConnection(dbConnection)
+    {}
+    void openConnection()
+    {
+        usedConnection->Open();
+    }
+
+    void useConnection()
+    {
+        usedConnection->execQuery();
+    }
+
+    void closeConnection()
+    {
+        usedConnection->Close();
+    }
+
+private:
+    DBConnectionInterface* usedConnection;
+};
+
+class MockConnection : public DBConnectionInterface {
+public:
+    // пользуемся макросами GTest для определения мок-методов в объекте
+    MOCK_METHOD(void, Open, (), (override));
+    MOCK_METHOD(void, execQuery, (), (override));
+    MOCK_METHOD(void, Close, (), (override));
+};
+
+TEST(ConnectionTest, testOpen)
+{
+    MockConnection mdbconnection;
+    classThatUsesDB oClassThatUsesDB(&mdbconnection);
+
+    EXPECT_CALL(mdbconnection, Open).Times(::testing::AtLeast(1));
+   
+    oClassThatUsesDB.openConnection();
+
+}
+
+TEST(ConnectionTest, testUseConnection)
+{
+    MockConnection mdbconnection;
+    classThatUsesDB oClassThatUsesDB(&mdbconnection);
+
+    EXPECT_CALL(mdbconnection, execQuery).Times(::testing::AtLeast(1));
+
+    oClassThatUsesDB.useConnection();
+
+}
+
+TEST(ConnectionTest, testClose)
+{
+    MockConnection mdbconnection;
+    classThatUsesDB oClassThatUsesDB(&mdbconnection);
+
+    EXPECT_CALL(mdbconnection, Close).Times(::testing::AtLeast(1));
+
+    oClassThatUsesDB.closeConnection();
+}
+
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
